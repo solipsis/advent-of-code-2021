f2b4f6f576bed0f9f0b1c7dca5b3870d49426abc
dave <dave.alexander@hey.com>
2021-12-13 19:16:39 -0700

    sample 1 working

--- 12/main.zig
+++ 12/main.zig
@@ -6,18 +6,16 @@ const test_allocator = std.testing.allocator;
 const expect = std.testing.expect;
 
 pub fn parse(buffer: []const u8) !StringHashMap(ArrayList([]const u8)) {
-    //var list = ArrayList(ArrayList([]const u8).init(test_allocator);
-
     var map = StringHashMap(ArrayList([]const u8)).init(test_allocator);
     var row_it = std.mem.split(buffer, "\n");
     while (row_it.next()) |raw_row| {
         var entry_it = std.mem.split(raw_row, "-");
-        var lhs: []const u8 = try map.allocator.dupe(u8, entry_it.next().?);
-        var rhs: []const u8 = try map.allocator.dupe(u8, entry_it.next().?);
+        var lhs: []const u8 = entry_it.next().?;
+        var rhs: []const u8 = entry_it.next().?;
 
-        std.debug.print("------------------------\n", .{});
-        std.debug.print("Raw_lhs: {s}\n", .{lhs});
-        std.debug.print("Raw_rhs: {s}\n", .{rhs});
+        //        std.debug.print("------------------------\n", .{});
+        //       std.debug.print("Raw_lhs: {s}\n", .{lhs});
+        //      std.debug.print("Raw_rhs: {s}\n", .{rhs});
 
         if (!map.contains(lhs)) {
             try map.put(lhs, ArrayList([]const u8).init(test_allocator));
@@ -28,32 +26,65 @@ pub fn parse(buffer: []const u8) !StringHashMap(ArrayList([]const u8)) {
 
         // std.debug.print("lhs_arr: {any}\n", .{map.get(lhs).?});
         //std.debug.print("lhs_arr: {any}\n", .{map.get(rhs).?});
-        std.debug.print("pre-append: lhs: {s} : {any} \n", .{ lhs, map.get(lhs).?.items });
-        std.debug.print("pre-append: rhs: {s} : {any} \n", .{ rhs, map.get(rhs).?.items });
+        //     std.debug.print("pre-append: lhs: {s} : {any} \n", .{ lhs, map.get(lhs).?.items });
+        //     std.debug.print("pre-append: rhs: {s} : {any} \n", .{ rhs, map.get(rhs).?.items });
         try map.getPtr(lhs).?.append(rhs);
         try map.getPtr(rhs).?.append(lhs);
-        std.debug.print("lhs: {s} : {any} \n", .{ lhs, map.get(lhs).?.items });
-        std.debug.print("rhs: {s} : {any} \n", .{ rhs, map.get(rhs).?.items });
+        //    std.debug.print("lhs: {s} : {any} \n", .{ lhs, map.get(lhs).?.items });
+        //   std.debug.print("rhs: {s} : {any} \n", .{ rhs, map.get(rhs).?.items });
 
-        //  std.debug.print("lhs: {s}, rhs: {s}\n", .{ lhs, rhs });
-        //  std.debug.print("lhs2: {}, rhs2: {}\n", .{ map.get(lhs).?.items.len, map.get(rhs).?.items.len });
-        //        map.put(lhs:
     }
-
-    var map_it = map.iterator();
-    //while (map_it.next()) |entry| {
-    //   std.debug.print("entry: {s}\n", .{entry.key_ptr.*});
-    //  std.debug.print("blah: {any}\n", .{entry.value_ptr.*});
-    //  for (entry.value_ptr.*.*.items) |item| {
-    //     std.debug.print("| {any}, ", .{item});
-    // }
-    // std.debug.print("\n", .{});
-    //std.debug.print("entry: {s}, {any}\n", .{ entry.key_ptr.*, entry.value_ptr.*.items });
-    //  entry.value_ptr.deinit();
-    //}
 
     return map;
 }
+
+pub fn isLowercase(str: []const u8) bool {
+    for (str) |byte| {
+        if (byte >= 'A' and byte <= 'Z') {
+            return false;
+        }
+    }
+    return true;
+}
+
+pub fn explore(caves: StringHashMap(ArrayList([]const u8)), cur_path: *ArrayList([]const u8)) AllocationError!usize {
+
+    // if at "end" print out path, then pop() and return
+    if (std.mem.eql(u8, cur_path.items[cur_path.items.len - 1], "end")) {
+        return 1;
+    }
+
+    var complete_paths: usize = 0;
+    var last: []const u8 = cur_path.items[cur_path.items.len - 1];
+    outer: for (caves.get(last).?.items) |neighbor| {
+        std.debug.print("visiting: {s}\n", .{neighbor});
+        // don't visit lowercase twice
+        if (isLowercase(neighbor)) {
+            for (cur_path.items) |item| {
+                if (std.mem.eql(u8, item, neighbor)) {
+                    continue :outer;
+                }
+            }
+        }
+        try cur_path.append(neighbor);
+        complete_paths += try explore(caves, cur_path);
+        _ = cur_path.pop();
+    }
+
+    // for each connected cave in caves.get(cur)
+    // if lowercase and cur_path contains it already skip
+    // explore(connected)
+    // pop()
+    //
+    // shouldn't need to worry about freeing path strings because they are tied to buffer
+    // still need to deinit ArrayList from main
+
+    return complete_paths;
+}
+
+const AllocationError = error{
+    OutOfMemory,
+};
 
 test "sample 1" {
     std.debug.print("\n", .{});
@@ -72,48 +103,27 @@ test "sample 1" {
     var trimmed = std.mem.trim(u8, buf, "\n"); // file trailing newline
 
     var m = try parse(trimmed);
-    defer m.deinit();
+    defer {
+        var it = m.iterator();
+        while (it.next()) |entry| {
+            entry.value_ptr.*.deinit();
+        }
+        m.deinit();
+    }
 
     var map_it = m.iterator();
     while (map_it.next()) |entry| {
         std.debug.print("entry: {s}\n", .{entry.key_ptr.*});
-        // std.debug.print("val: {any}\n", .{entry.value_ptr.*});
         for (entry.value_ptr.*.items) |item| {
             std.debug.print("| {s}, ", .{item});
         }
         std.debug.print("\n", .{});
     }
-    //std.debug.print("entry: {s}, {any}\n", .{ entry.key_ptr.*, entry.value_ptr.*.items });
-    //  entry.value_ptr.deinit();
-    //  }
 
-    // for (map.entries) {
+    var cur_path = ArrayList([]const u8).init(test_allocator);
+    defer cur_path.deinit();
 
-    //}
-}
-
-pub fn parse2(buffer: []const u8) !StringHashMap(*ArrayList([]const u8)) {
-    var map = StringHashMap(*ArrayList([]const u8)).init(test_allocator);
-    var row_it = std.mem.split(buffer, "\n");
-    while (row_it.next()) |raw_row| {
-        var entry_it = std.mem.split(raw_row, "-");
-        var lhs: []const u8 = try map.allocator.dupe(u8, entry_it.next().?);
-        var rhs: []const u8 = try map.allocator.dupe(u8, entry_it.next().?);
-
-        // allocate new list for key if it is a new key
-        if (!map.contains(lhs)) {
-            var new_arr = ArrayList([]const u8).init(test_allocator);
-            try map.put(lhs, &new_arr);
-        }
-        if (!map.contains(rhs)) {
-            var new_arr = ArrayList([]const u8).init(test_allocator);
-            try map.put(rhs, &new_arr);
-        }
-        try map.get(lhs).?.*.append(rhs);
-        try map.get(rhs).?.*.append(lhs);
-
-        std.debug.print("lhs: key: {s}  {*}\n", .{ lhs, map.get(lhs).? });
-        std.debug.print("rhs: key: {s}  {*}\n", .{ rhs, map.get(rhs).? });
-    }
-    return map;
+    try cur_path.append("start");
+    var num_paths = try explore(m, &cur_path);
+    std.debug.print("sample 1: {}\n", .{num_paths});
 }
