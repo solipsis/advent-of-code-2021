e2b8ebff8ee61ed7a7a74369db71680cbe3bbcc4
dave <dave.alexander@hey.com>
2021-12-16 10:55:12 -0700

    day 15 both samples

--- 15/main.zig
+++ 15/main.zig
@@ -8,27 +8,48 @@ const Grid = struct {
     risk: ArrayList(usize),
 };
 
-pub fn parse(reader: anytype) !Grid {
-    var buf = try reader.readAllAlloc(test_allocator, 999999);
-    defer test_allocator.free(buf);
-    var trimmed = std.mem.trim(u8, buf, "\n");
+const INF = 999999999;
 
-    var risk = ArrayList(usize).init(test_allocator);
-    var width: usize = 0;
+pub fn hyper_grid(orig: Grid) !Grid {
+    var hyper_buf = try test_allocator.alloc(usize, orig.risk.items.len * 25);
+    std.mem.set(usize, hyper_buf, 0);
+    defer test_allocator.free(hyper_buf);
 
-    var line_it = std.mem.split(trimmed, "\n");
-    while (line_it.next()) |line| {
-        width = line.len;
-        for (line) |byte| {
-            var i: usize = try std.fmt.charToDigit(byte, 10);
-            try risk.append(i);
+    var hyper_width = orig.width * 5;
+
+    //var col_offset: usize = 0;
+    //var row_offset: usize = 0;
+
+    var hyper_row: usize = 0;
+    while (hyper_row < 5) : (hyper_row += 1) {
+        var hyper_col: usize = 0;
+        while (hyper_col < 5) : (hyper_col += 1) {
+            for (orig.risk.items) |item, idx| {
+                var sub_col: usize = idx % orig.width;
+                var sub_row: usize = @divFloor(idx, orig.width);
+
+                var hyper_row_offset = hyper_row * hyper_width * (orig.risk.items.len / orig.width);
+                var hyper_col_offset = hyper_col * orig.width;
+                var hyper_idx = hyper_row_offset + hyper_col_offset + (sub_row * hyper_width) + sub_col;
+                //std.debug.print("orig: {}, h_row: {}, h_col: {}\n", .{ orig.risk.items[idx], hyper_row, hyper_col });
+                //std.debug.print("add: {}, mod: {}\n", .{ orig.risk.items[idx] + hyper_row + hyper_col, (orig.risk.items[idx] + hyper_row + hyper_col) % 9 });
+                var adjusted_val = (orig.risk.items[idx] + hyper_row + hyper_col);
+                if (adjusted_val > 9) {
+                    adjusted_val -= 9;
+                }
+                //if (adjusted_val == 0) {
+                //adjusted_val = 1;
+                //}
+                hyper_buf[hyper_idx] = adjusted_val;
+            }
         }
     }
 
-    return Grid{ .width = width, .risk = risk };
-}
+    var hyper_arr = ArrayList(usize).init(test_allocator);
+    try hyper_arr.appendSlice(hyper_buf);
 
-const INF = 999999999;
+    return Grid{ .width = hyper_width, .risk = hyper_arr };
+}
 
 pub fn update(risk: []usize, width: usize, cost: []usize, idx: usize) void {
     // create cost grid same size as risk // TODO: initialize outside of this
@@ -88,6 +109,16 @@ test "part 1" {
 
     update(grid.risk.items, grid.width, initial_cost, 0);
     std.debug.print("Part 1: {}\n", .{initial_cost[initial_cost.len - 1]});
+
+    var hyper = try hyper_grid(grid);
+    defer hyper.risk.deinit();
+
+    var hyper_initial_cost = try test_allocator.alloc(usize, hyper.risk.items.len);
+    defer test_allocator.free(hyper_initial_cost);
+    std.mem.set(usize, hyper_initial_cost, INF);
+
+    //update(hyper.risk.items, hyper.width, hyper_initial_cost, 0);
+    //std.debug.print("Part 2: {}\n", .{hyper_initial_cost[hyper_initial_cost.len - 1]});
 }
 
 test "sample 1" {
@@ -122,6 +153,27 @@ test "sample 1" {
 
     update(grid.risk.items, grid.width, initial_cost, 0);
     try expect(initial_cost[initial_cost.len - 1] == 40);
+    std.debug.print("Sample 1: {}\n", .{initial_cost[initial_cost.len - 1]});
+
+    var hyper = try hyper_grid(grid);
+    defer hyper.risk.deinit();
+
+    var hyper_initial_cost = try test_allocator.alloc(usize, hyper.risk.items.len);
+    defer test_allocator.free(hyper_initial_cost);
+    std.mem.set(usize, hyper_initial_cost, INF);
+
+    for (hyper.risk.items) |val, idx| {
+        std.debug.print("{}", .{val});
+        if ((idx + 1) % hyper.width == 0) {
+            //std.debug.print("idx: {}\n", .{idx});
+            std.debug.print("\n", .{});
+        }
+    }
+    //std.debug.print("\nsize: {}\n", .{hyper.risk.items.len});
+    //std.debug.print("\nwidth: {}\n", .{hyper.width});
+
+    update(hyper.risk.items, hyper.width, hyper_initial_cost, 0);
+    std.debug.print("Sample 2: {}\n", .{hyper_initial_cost[hyper_initial_cost.len - 1]});
 
     //   for (initial_cost) |val, idx| {
     //std.debug.print("{},", .{val});
@@ -129,4 +181,24 @@ test "sample 1" {
     //std.debug.print("\n", .{});
     //}
     //   }
+}
+
+pub fn parse(reader: anytype) !Grid {
+    var buf = try reader.readAllAlloc(test_allocator, 999999);
+    defer test_allocator.free(buf);
+    var trimmed = std.mem.trim(u8, buf, "\n");
+
+    var risk = ArrayList(usize).init(test_allocator);
+    var width: usize = 0;
+
+    var line_it = std.mem.split(trimmed, "\n");
+    while (line_it.next()) |line| {
+        width = line.len;
+        for (line) |byte| {
+            var i: usize = try std.fmt.charToDigit(byte, 10);
+            try risk.append(i);
+        }
+    }
+
+    return Grid{ .width = width, .risk = risk };
 }
