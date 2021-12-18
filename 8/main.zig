dc0ac5e3bca3064b59be84396f5cb4b042679069
dave <dave.alexander@hey.com>
2021-12-17 19:02:28 -0700

    finally 8 part 2

--- 8/main.zig
+++ 8/main.zig
@@ -125,16 +125,31 @@ pub fn parse(buffer: []const u8) !ArrayList(Entry) {
 }
 
 test "part 1" {
-    //std.debug.print("\n", .{});
+    std.debug.print("\n", .{});
 
-    //var file = try std.fs.cwd().openFile("input.txt", .{ .read = true });
-    //defer file.close();
+    var file = try std.fs.cwd().openFile("input.txt", .{ .read = true });
+    defer file.close();
 
-    //var entries = try parse(file.reader());
+    var buf = try file.reader().readAllAlloc(test_allocator, 999999);
+    defer test_allocator.free(buf);
 
-    //for (entries.items) |entry| {
-    //std.debug.print("{s}\n", .{entry.digits});
-    //}
+    var entries = try parse(buf);
+    defer {
+        for (entries.items) |entry| {
+            entry.digits.deinit();
+            entry.display.deinit();
+        }
+        entries.deinit();
+    }
+
+    var sum: usize = 0;
+    for (entries.items) |entry| {
+        std.debug.print("Start: {s}\n", .{entry.digits.items[0]});
+        std.sort.sort([]const u8, entry.digits.items, {}, cmpByLen);
+        std.debug.print("sorted: {any}\n", .{entry.digits.items});
+        sum += try solve(entry.digits.items, entry.display.items);
+    }
+    std.debug.print("sum: {}\n", .{sum});
 }
 
 // 0 -> 0, 2, 3, 5, 6, 7, 8, 9
@@ -159,7 +174,84 @@ pub fn intersect(set: *AutoHashMap(u8, void), vals: []const u8) void {
     }
 }
 
-pub fn solve(digits: [][]const u8, display_digits: [][]const u8) !void {
+pub fn retain(set: *AutoHashMap(u8, void), vals: []const u8) void {
+    var keys = set.keyIterator();
+    while (keys.next()) |key| {
+        var keep = false;
+        for (vals) |byte| {
+            if (byte == key.*) {
+                keep = true;
+            }
+        }
+        // Todo: can I do this while iterating?
+        if (!keep) {
+            _ = set.remove(key.*);
+        }
+    }
+}
+
+// remove all entries in b from a
+pub fn intersectSet(a: *AutoHashMap(u8, void), b: *AutoHashMap(u8, void)) void {
+    var keys = b.keyIterator();
+    while (keys.next()) |key| {
+        _ = a.remove(key.*);
+    }
+}
+
+//pub fn retainSet(
+
+//pub fn oops(sets: []AutoHashMap(u8, void)) bool {
+//for (sets) |set| {
+//if (sets.count == 0) {
+//unreachable;
+//}
+//}
+//}
+
+pub fn getLast(set: *AutoHashMap(u8, void)) u8 {
+    var last_key_it = set.keyIterator();
+    var last_key = [1]u8{last_key_it.next().?.*};
+    return last_key[0];
+}
+
+pub fn contains(digit_str: []const u8, needle: u8) bool {
+    //  std.debug.print("Contains: digit_str: {s}, needle: {c}\n", .{ digit_str, needle });
+    for (digit_str) |byte| {
+        if (byte == needle) {
+            //         std.debug.print("Contains: {any}\n", .{true});
+            return true;
+        }
+    }
+    //std.debug.print("Contains: {any}\n", .{false});
+    return false;
+}
+
+pub fn printSets(sets: [7]AutoHashMap(u8, void)) void {
+    std.debug.print("----------------------\nPrintSets\n", .{});
+
+    var idx: usize = 0;
+    while (idx < 7) : (idx += 1) {
+        std.debug.print("set[{}]: ", .{idx});
+        var set_it = sets[idx].keyIterator();
+        while (set_it.next()) |key_byte| {
+            std.debug.print("{c}", .{key_byte.*});
+        }
+        std.debug.print("\n", .{});
+    }
+}
+
+pub fn getLastStr(set: *AutoHashMap(u8, void)) []const u8 {
+    var last_key_it = set.keyIterator();
+    var last_key = [1]u8{last_key_it.next().?.*};
+    var blah = last_key[0..];
+
+    std.debug.print("Last_string: {s}\n", .{blah});
+    // std.debug.print("bool: {any}\n", .{std.mem.containsAtLeast(u8, "abcdeg", 1, blah)});
+    //
+    return blah;
+}
+
+pub fn solve(digits: [][]const u8, display_digits: [][]const u8) !usize {
     var sets = [_]AutoHashMap(u8, void){undefined} ** 7;
     for (sets) |set, idx| {
         sets[idx] = AutoHashMap(u8, void).init(test_allocator);
@@ -185,6 +277,10 @@ pub fn solve(digits: [][]const u8, display_digits: [][]const u8) !void {
             intersect(&sets[3], digit);
             intersect(&sets[4], digit);
             intersect(&sets[6], digit);
+            retain(&sets[2], digit);
+            retain(&sets[5], digit);
+            std.debug.print("After 1\n", .{});
+            printSets(sets);
         }
         // seven uses 0 / 2 / 5
         if (digit.len == 3) {
@@ -192,6 +288,13 @@ pub fn solve(digits: [][]const u8, display_digits: [][]const u8) !void {
             intersect(&sets[3], digit);
             intersect(&sets[4], digit);
             intersect(&sets[6], digit);
+            retain(&sets[0], digit);
+
+            // slot 0 is the character in 1 but not 7
+            intersectSet(&sets[0], &sets[2]);
+            intersectSet(&sets[0], &sets[5]);
+            std.debug.print("After 7\n", .{});
+            printSets(sets);
         }
         // 4 uses 1 / 2 / 3 /5
         if (digit.len == 4) {
@@ -217,61 +320,93 @@ pub fn solve(digits: [][]const u8, display_digits: [][]const u8) !void {
                 _ = missing.remove(byte);
             }
             var mis_val = missing.keyIterator().next().?.*;
+            std.debug.print("missing: {c}\n", .{mis_val});
             intersect(&sets[0], &[1]u8{mis_val});
             intersect(&sets[1], &[1]u8{mis_val});
             intersect(&sets[5], &[1]u8{mis_val});
             intersect(&sets[6], &[1]u8{mis_val});
         }
-    }
 
-    for (digits) |digit| {
-        if (digit.len == 6) {
-            // if we don't don't have 1 of the digits in from 2 / 5
-            // then this number is six and the missing digit is 2
-            var dig_key_it = sets[2].keyIterator();
-            var possible = [2]u8{ dig_key_it.next().?.*, dig_key_it.next().?.* };
-
-            var a = dig_key_it.next().?.*;
-            var contains_a = false;
-            for (digit) |char| {
-                if (char == a) {
-                    contains_a = true;
+        // propagate any sets with onl 1 item
+        {
+            var idx: usize = 0;
+            while (idx < 7) : (idx += 1) {
+                if (sets[idx].count() == 1) {
+                    var ctr: usize = 0;
+                    while (ctr < 7) : (ctr += 1) {
+                        if (ctr == idx) {
+                            continue;
+                        }
+                        var val = sets[idx].keyIterator().next().?.*;
+                        _ = sets[ctr].remove(val);
+                    }
                 }
-            }
-            var b = dig_key_it.next().?.*;
-            var contains_b = false;
-            for (digit) |char| {
-                if (char == b) {
-                    contains_b = true;
-                }
-            }
-
-            if (contains_a and contains_b) {
-                continue;
-            }
-            // a must be 2
-            if (!contains_a) {
-                //std.debug.print("a: {c}, b: {c}\n", a);
-                _ = sets[2].remove(b);
-                _ = sets[5].remove(a);
-            }
-            if (!contains_b) {
-                //std.debug.print("a: {c}, b: {c}\n");
-                _ = sets[2].remove(a);
-                _ = sets[5].remove(b);
             }
         }
     }
 
+    {
+        std.debug.print("\n----------------------\nFirst Segment\n", .{});
+
+        var idx: usize = 0;
+        while (idx < 7) : (idx += 1) {
+            std.debug.print("set[{}]: ", .{idx});
+            var set_it = sets[idx].keyIterator();
+            while (set_it.next()) |key_byte| {
+                std.debug.print("{c}", .{key_byte.*});
+            }
+            std.debug.print("\n", .{});
+        }
+    }
+
+    //  for (digits) |digit| {
+    //if (digit.len == 6) {
+    //// if we don't don't have 1 of the digits in from 2 / 5
+    //// then this number is six and the missing digit is 2
+    //var dig_key_it = sets[2].keyIterator();
+    //var possible = [2]u8{ dig_key_it.next().?.*, dig_key_it.next().?.* };
+
+    //var a = dig_key_it.next().?.*;
+    //var contains_a = false;
+    //for (digit) |char| {
+    //if (char == a) {
+    //contains_a = true;
+    //}
+    //}
+    //var b = dig_key_it.next().?.*;
+    //var contains_b = false;
+    //for (digit) |char| {
+    //if (char == b) {
+    //contains_b = true;
+    //}
+    //}
+
+    //if (contains_a and contains_b) {
+    //continue;
+    //}
+    //// a must be 2
+    //if (!contains_a) {
+    ////std.debug.print("a: {c}, b: {c}\n", a);
+    //_ = sets[2].remove(b);
+    //_ = sets[5].remove(a);
+    //}
+    //if (!contains_b) {
+    ////std.debug.print("a: {c}, b: {c}\n");
+    //_ = sets[2].remove(a);
+    //_ = sets[5].remove(b);
+    //}
+    //}
+    //}
+
     // 4 and 6 are leftover from the 3 above unique cases
-    var key_it = sets[4].keyIterator();
-    var key = [2]u8{ key_it.next().?.*, key_it.next().?.* };
-    std.debug.print("key: {s}\n", .{key[0..]});
-    intersect(&sets[0], key[0..]);
-    intersect(&sets[1], key[0..]);
-    intersect(&sets[2], key[0..]);
-    intersect(&sets[3], key[0..]);
-    intersect(&sets[5], key[0..]);
+    //var key_it = sets[4].keyIterator();
+    //var key = [2]u8{ key_it.next().?.*, key_it.next().?.* };
+    ////std.debug.print("key: {s}\n", .{key[0..]});
+    //intersect(&sets[0], key[0..]);
+    //intersect(&sets[1], key[0..]);
+    //intersect(&sets[2], key[0..]);
+    //intersect(&sets[3], key[0..]);
+    //intersect(&sets[5], key[0..]);
 
     // 1 and 3 leftover
     //  key_it = sets[1].keyIterator();
@@ -293,56 +428,129 @@ pub fn solve(digits: [][]const u8, display_digits: [][]const u8) !void {
     intersect(&sets[5], last_key[0..]);
     intersect(&sets[6], last_key[0..]);
 
-    last_key_it = sets[2].keyIterator();
-    last_key = [1]u8{last_key_it.next().?.*};
-    intersect(&sets[5], last_key[0..]);
+    //  last_key_it = sets[2].keyIterator();
+    //  last_key = [1]u8{last_key_it.next().?.*};
+    //  intersect(&sets[5], last_key[0..]);
 
-    var idx: usize = 0;
-    while (idx < 7) : (idx += 1) {
-        if (sets[idx].count() == 1) {
-            var ctr: usize = 0;
-            while (ctr < 7) : (ctr += 1) {
-                if (ctr == idx) {
-                    continue;
+    {
+        var idx: usize = 0;
+        while (idx < 7) : (idx += 1) {
+            if (sets[idx].count() == 1) {
+                var ctr: usize = 0;
+                while (ctr < 7) : (ctr += 1) {
+                    if (ctr == idx) {
+                        continue;
+                    }
+                    var val = sets[idx].keyIterator().next().?.*;
+                    _ = sets[ctr].remove(val);
                 }
-                var val = sets[idx].keyIterator().next().?.*;
-                _ = sets[ctr].remove(val);
             }
         }
     }
 
-    idx = 0;
-    while (idx < 7) : (idx += 1) {
-        std.debug.print("set[{}]: ", .{idx});
-        var set_it = sets[idx].keyIterator();
-        while (set_it.next()) |key_byte| {
-            std.debug.print("{c}", .{key_byte.*});
+    {
+        var idx: usize = 0;
+        while (idx < 7) : (idx += 1) {
+            std.debug.print("set[{}]: ", .{idx});
+            var set_it = sets[idx].keyIterator();
+            while (set_it.next()) |key_byte| {
+                std.debug.print("{c}", .{key_byte.*});
+            }
+            std.debug.print("\n", .{});
         }
-        std.debug.print("\n", .{});
     }
 
-    //var s0 = AutoHashMap(u8, void).init(allocator);
-    //try initSet(&s0);
-    //var s1 = AutoHashMap(u8, void).init(allocator);
-    //try initSet(&s0);
-    //var s2 = AutoHashMap(u8, void).init(allocator);
-    //try initSet(&s0);
-    //var s3 = AutoHashMap(u8, void).init(allocator);
-    //try initSet(&s0);
-    //var s4 = AutoHashMap(u8, void).init(allocator);
-    //try initSet(&s0);
-    //var s5 = AutoHashMap(u8, void).init(allocator);
-    //try initSet(&s0);
-    //var s6 = AutoHashMap(u8, void).init(allocator);
-    //try initSet(&s0);
+    var out = [4]u8{ 0, 0, 0, 0 };
+    for (display_digits) |display_digit, idx| {
+        std.debug.print("display_digit: {s}\n", .{display_digit});
+        if (display_digit.len == 2) {
+            out[idx] = '1';
+            continue;
+        }
+        if (display_digit.len == 3) {
+            out[idx] = '7';
+            continue;
+        }
+        if (display_digit.len == 4) {
+            out[idx] = '4';
+            continue;
+        }
+        if (display_digit.len == 7) {
+            out[idx] = '8';
+            continue;
+        }
 
+        if (display_digit.len == 6) {
+            var contains_2 = contains(display_digit, getLast(&sets[2]));
+            var contains_4 = contains(display_digit, getLast(&sets[4]));
+            if (contains_2 and contains_4) {
+                out[idx] = '0';
+                continue;
+            } else if (contains_2) {
+                out[idx] = '9';
+                continue;
+            } else if (contains_4) {
+                out[idx] = '6';
+                continue;
+            } else {
+                unreachable;
+            }
+            // zero
+
+            // six
+            // nine
+        } else {
+            var contains_1 = contains(display_digit, getLast(&sets[1]));
+            //var contains_debug = std.mem.containsAtLeast(u8, "cdfeb", 1, getLastStr(&sets[1]));
+            //var needle = getLastStr(&sets[1]);
+            //std.debug.print("Contains1: {any}, digit: {s}, needle: {s}, debug: {any}\n", .{ contains_1, display_digit, getLastStr(&sets[1]), contains_debug });
+            //std.debug.print("debug2: {any}, needle: {s}\n", .{ std.mem.containsAtLeast(u8, "cdfeb", 1, needle), needle });
+            if (contains_1) {
+                out[idx] = '5';
+                continue;
+            }
+            var contains_4 = contains(display_digit, getLast(&sets[4]));
+            if (contains_4) {
+                out[idx] = '2';
+                continue;
+            } else {
+                out[idx] = '3';
+                continue;
+            }
+
+            // var contains_
+            // 2
+            // 3
+            // 5
+        }
+    }
+    std.debug.print("Out: {s}\n", .{out});
+    return try std.fmt.parseUnsigned(usize, out[0..], 10);
+}
+
+pub fn cmpByLen(context: void, a: []const u8, b: []const u8) bool {
+    return a.len < b.len;
 }
 
 test "sample 1" {
     std.debug.print("\n", .{});
     const input =
-        \\acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf
+        \\be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
+        \\edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
+        \\fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg
+        \\fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb
+        \\aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea
+        \\fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb
+        \\dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe
+        \\bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef
+        \\egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
+        \\gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce
     ;
+
+    //const input = "edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc";
+    //const input = "acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf";
+
+    //const input = "fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb";
     var fbs = std.io.fixedBufferStream(input);
     var buf = try fbs.reader().readAllAlloc(test_allocator, 999999);
     defer test_allocator.free(buf);
@@ -356,10 +564,14 @@ test "sample 1" {
         entries.deinit();
     }
 
+    var sum: usize = 0;
     for (entries.items) |entry| {
-        std.debug.print("{s}\n", .{entry.digits.items[0]});
+        std.debug.print("Start: {s}\n", .{entry.digits.items[0]});
+        std.sort.sort([]const u8, entry.digits.items, {}, cmpByLen);
+        std.debug.print("sorted: {any}\n", .{entry.digits.items});
+        sum += try solve(entry.digits.items, entry.display.items);
     }
-    try solve(entries.items[0].digits.items, entries.items[0].display.items);
+    std.debug.print("sum: {}\n", .{sum});
 
     //  for (entries.items[0].display) |digit| {
     //    std.debug.print("display: {}\n", .{digit});
